8859ba89e0453230d1137fa41e72458bcfca8aa9
Genji2333 <1453708682@qq.com>
2025-08-01 17:12:55 +0800

    .

--- experiments/fast_rcnn/fast-rcnn_r50_fpn_1x_coco.py
+++ experiments/fast_rcnn/fast-rcnn_r50_fpn_1x_coco.py
@@ -1,7 +1,8 @@
 _base_ = [
     'mmdet::_base_/datasets/wheat.py',
     'mmdet::_base_/schedules/schedule_1x.py', 
-    'mmdet::_base_/default_runtime.py'
+    'mmdet::_base_/default_runtime.py',
+    'mmdet::_base_/fast_rcnn/fast-rcnn_r50_fpn_1x_coco.py'
 ]
 
 # model settings
