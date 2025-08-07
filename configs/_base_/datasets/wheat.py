dac2c8e2e4decac93ce41ea0f7ea71ea00e939a1
Genji2333 <1453708682@qq.com>
2025-08-07 16:49:50 +0800

    fix: update wheat dataset pipeline for Mask R-CNN

--- configs/_base_/datasets/wheat.py
+++ configs/_base_/datasets/wheat.py
@@ -9,14 +9,14 @@ backend_args = None
 
 train_pipeline = [
     dict(type='LoadImageFromFile', backend_args=backend_args),
-    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='LoadAnnotations', with_bbox=True,with_mask=True),
     dict(type='Resize', scale=(640, 640), keep_ratio=True),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PackDetInputs')
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile', backend_args=backend_args),
-    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True,with_mask=True), # with_mask=True maskrcnn专用
     # If you don't have a gt annotation, delete the pipeline
     dict(type='LoadAnnotations', with_bbox=True),
     dict(
