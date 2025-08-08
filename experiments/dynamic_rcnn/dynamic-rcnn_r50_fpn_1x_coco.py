34c21aec58e7ce96155220f9693aad2e10a0012f
Genji2333 <1453708682@qq.com>
2025-08-08 15:36:16 +0800

    .

---
+++ experiments/dynamic_rcnn/dynamic-rcnn_r50_fpn_1x_coco.py
@@ -0,0 +1,56 @@
+_base_ = [
+    '../faster_rcnn/faster-rcnn_r50_fpn_1x_coco.py',
+    'mmdet::_base_/datasets/wheat.py',
+    'mmdet::_base_/schedules/schedule_1x.py', 
+    'mmdet::_base_/default_runtime.py'
+]
+model = dict(
+    roi_head=dict(
+        type='DynamicRoIHead',
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=1,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))),
+    train_cfg=dict(
+        rpn_proposal=dict(nms=dict(iou_threshold=0.85)),
+        rcnn=dict(
+            dynamic_rcnn=dict(
+                iou_topk=75,
+                beta_topk=10,
+                update_iter_interval=100,
+                initial_iou=0.4,
+                initial_beta=1.0))),
+    test_cfg=dict(rpn=dict(nms=dict(iou_threshold=0.85))))
+
+
+optim_wrapper = dict(
+    optimizer=dict(lr=0.01),
+    paramwise_cfg=dict(bias_lr_mult=2., bias_decay_mult=0.),
+    clip_grad=None)
+
+# learning rate # 学习率设置
+max_epochs = 12
+param_scheduler = [
+    dict(type='LinearLR', start_factor=0.1, by_epoch=False, begin=0, end=500),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=max_epochs,
+        by_epoch=True,
+        milestones=[8, 11],
+        gamma=0.1)
+]
+
+# 比如模型跑多少轮，几轮进行一下验证，也在schedule里
+
+train_cfg = dict(max_epochs=max_epochs) # 这里重写训练轮数
