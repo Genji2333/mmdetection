ddb9714af151c8a972ad34aa2531b3f83ef0e2c3
Genji2333 <1453708682@qq.com>
2025-08-13 15:25:00 +0800

    .

--- experiments/resnest/cascade-mask-rcnn_s50_fpn_syncbn-backbone+head_ms-1x_coco.py
+++ experiments/resnest/cascade-mask-rcnn_s50_fpn_syncbn-backbone+head_ms-1x_coco.py
@@ -1,7 +1,8 @@
 _base_ = [
     'mmdet::_base_/datasets/wheat.py',
     'mmdet::_base_/schedules/schedule_1x.py', 
-    'mmdet::_base_/default_runtime.py'
+    'mmdet::_base_/default_runtime.py',
+    'mmdet::_base_/cascade_rcnn/cascade-mask-rcnn_r50_fpn_1x_coco.py'
 ]
 
 norm_cfg = dict(type='SyncBN', requires_grad=True)
