010b54f2206ea59b7809e513249ef0d0a7065bf6
Genji2333 <1453708682@qq.com>
2026-02-04 19:12:50 +0800

    迁移到182

--- mmdet/evaluation/metrics/coco_metric.py
+++ mmdet/evaluation/metrics/coco_metric.py
@@ -71,7 +71,7 @@ class CocoMetric(BaseMetric):
                  ann_file: Optional[str] = None,
                  metric: Union[str, List[str]] = 'bbox',
                  classwise: bool = False,
-                 proposal_nums: Sequence[int] = (100, 300, 1000),
+                 proposal_nums: Sequence[int] = (1, 10, 100),
                  iou_thrs: Optional[Union[float, Sequence[float]]] = None,
                  metric_items: Optional[Sequence[str]] = None,
                  format_only: bool = False,
